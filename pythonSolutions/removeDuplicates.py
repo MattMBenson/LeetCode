abb1ff3f08d4a24764561a07153e61775f1562e0
MattMBenson <72677257+MattMBenson@users.noreply.github.com>
2021-07-11 22:05:22 -0400

    Correct debug but wrong on leetcode(?)

--- pythonSolutions/removeDuplicates.py
+++ pythonSolutions/removeDuplicates.py
@@ -3,6 +3,8 @@ class Solution(object):
         hashMap = {}
         i = 0 #count/key 
         k = 0 #amt of nums removed
+        # i + k = len(nums)
+        # k value to be returned
         for value in nums:
             if not (value in hashMap.values()):
                 hashMap[i] = value
@@ -10,7 +12,15 @@ class Solution(object):
             else:
                 k+=1
 
-        for x in range(0,i):
-            nums(i) = hashMap[i]
+        for x in range(0,len(nums)):
+            if x > i-1:
+                nums[x] = None
+            else: 
+                nums[x] = hashMap[x]
         return k
+
+test = Solution()
+nums = [1,1,2]
+print(test.removeDuplicates(nums))
+print(nums)
         
